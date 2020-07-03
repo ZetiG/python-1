88449a42cc92638a55966be8f2058d6d9408af02
zeti <zmk95511@163.com>
2020-07-03 15:13:40 +0800

    Initial commit

---
+++ First.py
@@ -0,0 +1,11 @@
+# 国际惯例 hello world
+print("hello python")
+
+# len() 字符串长度
+# := 赋值符号
+
+string = "python"
+if (n := len(string)) > 5:
+    print(f"List is too long ({n} elements, expected <= 5)")
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+### python 学习小结
+
+[![](https://img.shields.io/badge/language-python4.8-blue)](https://www.python.org/)
